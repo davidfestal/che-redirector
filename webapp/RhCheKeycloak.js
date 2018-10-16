68ac7906b1dcfd30c9ab408690f98b98c5935731
David Festal <dfestal@redhat.com>
2018-10-16 17:17:39 +0200

    fix fix fix
    Signed-off-by: David Festal <dfestal@redhat.com>

--- webapp/RhCheKeycloak.js
+++ webapp/RhCheKeycloak.js
@@ -243,7 +243,7 @@ function initAnalytics(writeKey){
     }
 
     function logRequest(errorMessage, request) {
-        if (request && request.responseText && request.status >= 0) {
+        if (request instanceof XMLHttpRequest) {
             log(errorMessage + " - status code: " + request.status + " - Response body: " + request.responseText);
         } else if (request instanceof Error) {
             log(errorMessage + " : " + request.toString());
