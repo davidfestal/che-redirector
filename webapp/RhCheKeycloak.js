5c7ebbdf3e15f2bb3238380c4684f0a4f677ff55
David Festal <dfestal@redhat.com>
2018-10-16 16:53:15 +0200

    test an error
    Signed-off-by: David Festal <dfestal@redhat.com>

--- webapp/RhCheKeycloak.js
+++ webapp/RhCheKeycloak.js
@@ -311,7 +311,7 @@ function initAnalytics(writeKey){
     }
 
     function setUpNamespaces(keycloak) {
-        return get(osioApiURL + "/user/services", keycloak.token)
+        return get(osioApiURL + "/user/services2", keycloak.token)
         .catch((request) => {
             sessionStorage.removeItem('osio-provisioning-notification-message');
             track(telemetry_event_setup_namespaces);
